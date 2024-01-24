4c867a6a395c61e5a01d2751ceac41366116c0ca
willotavio <willaotavio@gmail.com>
2024-01-23 22:56:52 -0300

    Add copy button in encryption_window

--- windows/encryption_window/encryption_window.py
+++ windows/encryption_window/encryption_window.py
@@ -14,6 +14,11 @@ class EncryptionWindow(BoxLayout):
         super(EncryptionWindow, self).__init__(**kwargs)
         self.ids.encrypt_button.bind(on_press=self.encrypt)
 
+    def on_copy_button_press(self):
+        if len(self.ids.encrypted_result.text) > 0:
+            Clipboard.copy(str(self.ids.encrypted_result.text))
+            self.ids.copy_confirmation.text = "Copied to clipboard \\(>-<*)|"
+
     def encrypt(self, button_instance):
         status, result = Cryptographer.encrypt(self, self.ids.encryption_key_input.text, self.ids.text_to_encrypt_input.text)
         encrypted_texts = ""
