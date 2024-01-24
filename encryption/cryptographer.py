68b3c35625298017e42a7f589c2691a9778c1853
willotavio <willaotavio@gmail.com>
2024-01-23 22:44:26 -0300

    Change encrypt method to handle multiple texts

--- encryption/cryptographer.py
+++ encryption/cryptographer.py
@@ -5,20 +5,25 @@ import binascii
 class Cryptographer:
 
     @staticmethod
-    def encrypt(self, encryption_key, text_to_encrypt):
+    def encrypt(self, encryption_key, texts_to_encrypt):
         try:
-            if encryption_key and len(encryption_key) == 16 and text_to_encrypt:
+            if encryption_key and len(encryption_key) == 16 and texts_to_encrypt:
+                encrypted_texts = []
                 encryption_key = encryption_key.encode('utf-8')
-                text_to_encrypt = text_to_encrypt.encode('utf-8')
-                cipher = AES.new(encryption_key, AES.MODE_EAX)
-                encrypted_text, tag = cipher.encrypt_and_digest(text_to_encrypt)
+                for text_to_encrypt in texts_to_encrypt.split(' | '):
+                    text_to_encrypt = text_to_encrypt.encode('utf-8')
+                    cipher = AES.new(encryption_key, AES.MODE_EAX)
+                    encrypted_text, tag = cipher.encrypt_and_digest(text_to_encrypt)
 
-                nonce = binascii.hexlify(cipher.nonce).decode()
-                encrypted_text = binascii.hexlify(encrypted_text).decode()
-                tag = binascii.hexlify(tag).decode()
+                    nonce = binascii.hexlify(cipher.nonce).decode()
+                    encrypted_text = binascii.hexlify(encrypted_text).decode()
+                    tag = binascii.hexlify(tag).decode()
 
-                encrypted_info = f"{nonce}/{encrypted_text}/{tag}"
-                return True, encrypted_info
+                    encrypted_info = f"{nonce}/{encrypted_text}/{tag}"
+                    encrypted_texts.append(encrypted_info)
+
+                return True, encrypted_texts
+
             else:
                 return False, "Password must have 16 characters (*•~•)л"
         except ValueError as e:
--- windows/encryption_window/encryption_window.py
+++ windows/encryption_window/encryption_window.py
@@ -1,4 +1,5 @@
 from kivy.uix.boxlayout import BoxLayout
+from kivy.core.clipboard import Clipboard
 from encryption.cryptographer import Cryptographer
 
 from kivy.lang import Builder
@@ -11,12 +12,19 @@ class EncryptionWindow(BoxLayout):
 
     def __init__(self, **kwargs):
         super(EncryptionWindow, self).__init__(**kwargs)
-
         self.ids.encrypt_button.bind(on_press=self.encrypt)
 
     def encrypt(self, button_instance):
         status, result = Cryptographer.encrypt(self, self.ids.encryption_key_input.text, self.ids.text_to_encrypt_input.text)
-        self.ids.encrypted_result.text = result
+        encrypted_texts = ""
         if status:
+            n = 1
+            for item in result:
+                encrypted_texts += f"{item}\n\n"
+                n += 1
+            encrypted_texts = encrypted_texts[:-1]
             self.ids.encryption_key_input.text = ""
             self.ids.text_to_encrypt_input.text = ""
+        else:
+            encrypted_texts = result
+        self.ids.encrypted_result.text = encrypted_texts
